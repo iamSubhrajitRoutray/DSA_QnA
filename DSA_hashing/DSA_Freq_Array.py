4e17d00d49b7a1276928714bef9d2a84e5f6e03c
Subhrajit Routray <theroutraysubhrajit@gmail.com>
2026-05-10 08:38:17 +0530

    Add files via upload

---
+++ DSA_hashing/DSA_Freq_Array.py
@@ -0,0 +1,29 @@
+'''Count frequency of each element in the array'''
+
+# defining the function
+def frequency(arr, num):
+    
+    visited = [False] * num     # making an array to keep in check if we've visited that num/index
+    
+    for i in range(num):
+    
+        if visited[i]:     # continue if True 
+            continue
+        
+        count = 1      # initiliazing the counter to 1
+    
+        for j in range(i + 1, num):       # j will loop from i+1 to avoid repeatation of indexes
+    
+            if arr[i] == arr[j]:
+    
+                visited[j] = True      # will increment by 1 if we find the same num in the given array
+                count += 1
+                
+        print(f'{arr[i]} : {count}')
+        
+
+arr = [5,10,100,15,10,50,5,15]
+
+num = len(arr)
+
+frequency(arr, num)
---
+++ DSA_hashing/DSA_Min_Max_Freq.py
@@ -0,0 +1,62 @@
+'''Find the highest/lowest frequency element'''
+
+def frequency(arr, num):
+   
+    # Track if an element has already been counted
+    visted = [False] * num
+   
+    # Store max frequency seen so far
+    max_freq = 0
+   
+    # Store min frequency (start high)
+    min_freq = num
+   
+    # Element with max frequency
+    max_ele = 0
+   
+    # Element with min frequency
+    min_ele = 0
+    
+    for i in range(num):
+        
+        # Skip already processed elements
+        if visted[i]:
+            continue
+        
+        # Count frequency of arr[i]
+        count = 1
+        for j in range(i + 1, num):
+            if arr[i] == arr[j]:
+                
+                # Mark as visited
+                visted[j] = True
+                
+                # increment the counter
+                count += 1
+        
+        # Update the max frequency
+        if count > max_freq:
+            max_ele = arr[i]
+            max_freq = count
+            
+        # Update the min frequency   
+        if count < min_freq:
+            min_ele = arr[i]
+            min_freq = count
+            
+        # print all the elemnets with their respective frequency
+        print(f'{arr[i]} : {count}')
+        
+    # print the max and min element
+    print(f'The element with maximum freq is {max_ele} having {max_freq} no. of occurence')
+    
+    print(f'The element with minimum freq is {min_ele} having {min_freq} no. of occurence')
+    
+
+# Main/Driver code
+arr = [1,3,1,15,3,1,5,1]
+# Get array length
+n = len(arr)
+# Function call
+frequency(arr, n)
+            
