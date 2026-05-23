8df37b17d387a8b658d208cc4a14ddd83759ea91
Subhrajit Routray <250405310+iamSubhrajitRoutray@users.noreply.github.com>
2026-05-23 14:09:08 +0530

    Add files via upload

---
+++ DSA_Array_Medium/DSA_Next_Permutation.py
@@ -0,0 +1,42 @@
+def next_permutation(arr):
+    
+    n = len(arr)
+    
+    index = -1
+    
+    for i in range(n - 2, -1, -1):
+        
+        if arr[i] < arr[i + 1]:
+            
+            index = i
+            break
+        
+    
+    if index == -1:
+        
+        arr.reverse()
+        return
+    
+    
+    for i in range(n - 1, index, -1):
+        
+        if arr[i] > arr[index]:
+            
+            arr[i], arr[index] = arr[index], arr[i]
+            break
+        
+    
+    arr[index + 1: ] = reversed(arr[index + 1: ])
+    
+
+def run_code():
+    
+    array = [1,2,3]
+    
+    next_permutation(array)
+    
+    print(''.join(map(str,array)))
+    
+run_code()
+
+
