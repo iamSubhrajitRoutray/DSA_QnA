06358679c71549bcd3ae95928deaac9557520fb3
iamSubhrajitRoutray <theroutraysubhrajit@gmail.com>
2026-05-25 10:01:56 +0530

    dsa question to find leaders in an array

---
+++ DSA_Array_Medium/DSA_Leaders_Array.py
@@ -0,0 +1,29 @@
+'''Leaders in an Array'''
+
+
+'''OPTIMAL APPROACH'''
+
+def leader_array(arr):
+    
+    n = len(arr)
+    
+    ans = []
+    
+    leader = arr[-1]
+    
+    ans.append(leader)
+    
+    for i in range(n - 2, -1, -1):
+    
+        if arr[i] > arr[-1]:
+            
+            ans.append(arr[i])
+    
+    print(ans)
+
+
+# MAIN CODE ->
+array = [1, 8, 6, 4, 5]
+leader_array(array)
+            
+            
