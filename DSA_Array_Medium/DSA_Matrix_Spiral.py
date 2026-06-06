dbf1fa5ba89445bf934401666c5911d7096d0413
Subhrajit Routray <theroutraysubhrajit@gmail.com>
2026-06-06 08:57:36 +0530

    "Debugging and organzing the codes."

--- DSA_Array_Medium/DSA_Matrix_Spiral.py
+++ DSA_Array_Medium/DSA_Matrix_Spiral.py
@@ -2,6 +2,7 @@
 Q)
 Problem Statement: Given a Matrix, print the given matrix in spiral order.'''
 
+
 def spiral(matrix):
     
     top = 0
@@ -13,26 +14,27 @@ def spiral(matrix):
     
     while top <= bottom and left <= right:
         
-        for i in range(left, right+1):
+        for i in range(left, right + 1):
             result.append(matrix[top][i])
         top += 1
         
-        for i in range(top,bottom+1):
+        for i in range(top, bottom + 1):
             result.append(matrix[i][right])
         right -= 1
         
         if left <= right:
-            for i in range(right,left-1,-1):
+            for i in range(right, left - 1, -1):
                 result.append(matrix[bottom][i])
             bottom -= 1
             
         if top <= bottom:
-            for i in range(bottom,top-1,-1):
+            for i in range(bottom, top - 1, -1):
                 result.append(matrix[i][left])
             left += 1
             
     print(result)
         
+# Main/Driver code :
         
 matrix = [[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]]
 spiral(matrix)
