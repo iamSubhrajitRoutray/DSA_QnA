74e9a0f911b00139173419cdc5005a962b7312e1
Subhrajit Routray <theroutraysubhrajit@gmail.com>
2026-06-15 23:50:52 +0530

    "Debugging & Scrutinizing."

--- DSA_BasicMath/DSA_allDivisors.py
+++ DSA_BasicMath/DSA_allDivisors.py
@@ -1,28 +1,60 @@
 '''Q) Print all divisors of a given number'''
 
-def divisors(n):
-    
-    # Create list to store divisors
-    div = []
-    
-    for i in range(1, n + 1):
+class Divisors:
+    def find_divisors(self, n):
         
-        # Check if i is a divisor of N
+        # Create list to store divisors
+        div = []
         
-        if n % i == 0:
-            # Add i to result
+        for i in range(1, n + 1):
             
-            div.append(i)
-    
-    # Return the list of divisors
-    return div
+            # Check if i is a divisor of N
+            if n % i == 0:
+                
+                # Add i to result
+                div.append(i)
+        
+        # Return the list of divisors
+        return div
 
 
 # Main/Driver code : 
-num = int(input('enter the number: '))
+num = int(input("Enter the number: "))
 
-result = divisors(num)
+obj = Divisors()
 
-print(f'Divisors are : {result}.')
+result = obj.find_divisors(num)
 
+print(f"Divisors are : {result}.")
+
+
+
+
+'''Implementing OOPs conpects->'''
+
+class Find_Divisors:
+    
+    def __init__(self, num):
+         self.num = num
+         self.div = []
+    
+    def divisors(self):
+        
+        for i in range(1, self.num + 1):
+            
+            if self.num % i == 0:
+                self.div.append(i)
+                
+    def display(self):
+        print(f"Divisors of {self.num} are {self.div}.")
+        
+
+# Main/Driver code :
+
+number = int(input("Enter the number : "))
+
+finder = Find_Divisors(number)
+
+finder.divisors()
+finder.display()
     
