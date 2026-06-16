6c2b30b997004b87393a470a771c62d63a530119
Subhrajit Routray <theroutraysubhrajit@gmail.com>
2026-06-16 17:06:09 +0530

    "Debugging & Scrutinizing."

--- DSA_BasicMath/DSA_Palindrome.py
+++ DSA_BasicMath/DSA_Palindrome.py
@@ -33,3 +33,88 @@ if check_num == num:
 
 else:
     print(f'{num} is not a Palindrome.')
+    
+    
+    
+    
+'''Implementing OOPs concepts ->'''
+
+class Palindrome:
+    
+    def __init__(self, num):
+        
+        self.num = num
+        
+    def check_palindrome(self):
+        
+        result_num = 0
+        
+        temp_num = self.num
+        
+        while temp_num > 0:
+            
+            rev_num = temp_num % 10
+            
+            result_num = result_num * 10 + rev_num
+            
+            temp_num //= 10
+            
+        return result_num 
+    
+    def display(self):
+        
+        check_num = self.check_palindrome()
+        
+        if check_num == self.num:
+            print(f"YES, the number is a palindrome.")
+        
+        else:
+            print(f"NO, the number is not a palindrome.")
+
+
+# Main/Driver code : 
+       
+number =int(input("Enter the number : "))
+
+answer = Palindrome(number)
+
+answer.display()
+
+
+
+'''OR : we can also implement our OOPs concepts in a more better way ->'''
+
+
+class Palindrome:
+    
+    def __init__(self, num):
+        
+        self.num = num
+    
+    def check_plaindrome(self):
+    
+        result_num = 0
+        temp_num = self.num
+        
+        while temp_num > 0:
+            
+            rev_num = temp_num % 10
+            
+            result_num = result_num * 10 + rev_num
+            
+            temp_num //= 10
+            
+        return result_num == self.num
+
+
+# Main/Driver code :
+
+number = int(input("Enter the number : "))
+
+answer = Palindrome(number)
+
+if answer.check_plaindrome():
+    print(f"YES, the number is palindrome.")
+
+else:
+    print(f"NO, the number is not a palindrome.") 
