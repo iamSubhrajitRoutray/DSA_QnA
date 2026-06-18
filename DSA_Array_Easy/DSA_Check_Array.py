047755f546f512a53d685f165cef38ad926ff852
Subhrajit Routray <theroutraysubhrajit@gmail.com>
2026-06-18 10:03:39 +0530

    "Debugging & Scrutinizing."

--- DSA_Array_Easy/DSA_Check_Array.py
+++ DSA_Array_Easy/DSA_Check_Array.py
@@ -51,3 +51,94 @@ print(array)
 is_sorted(array)
 
 print('True' if is_sorted(array) else 'False')
+
+
+
+
+'''IMPLEMENTING OOPs CONCEPTS.'''
+
+
+# BRUTE FORCE APPROACH ->
+
+class Check_Array:
+    
+    def __init__(self, arr):
+        self.arr = arr
+
+    def is_sorted(self):
+        
+        num = len(self.arr)
+        
+        for i in range(0, num):
+            
+            for j in range(i + 1, num):
+            
+                if self.arr[j] < self.arr[i]:
+            
+                    return False
+            
+            return True
+    
+    def display(self):
+        
+        check = is_sorted(self.arr)
+        
+        print(f"Given array : {self.arr}")
+        
+        print("True" if check else "False")
+
+
+# Main/Driver code :
+
+array_1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+a1 = Check_Array(array_1)
+a1.display()
+
+array_2 = [1, 2, 3, 5, 7, 4, 6, 8, 9, 10]
+
+a2 = Check_Array(array_2)
+a2.display()
+
+
+
+
+# OPTIMAL APPROACH
+
+class Check_Array:
+    
+    def __init__(self, arr):
+        self.arr = arr
+        
+    def is_sorted(self):
+        
+        num = len(self.arr)
+        
+        for i in range(1, num):
+            
+            if self.arr[i] < self.arr[i-1]:
+                
+                return False
+        return True
+    
+    def display(self):
+        
+        check = self.is_sorted()
+        
+        print(f"Given array is : {self.arr}")
+        
+        print("True" if check else "False")
+        
+
+# Main/Driver code :
+
+array_1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+a1 = Check_Array(array_1)
+a1.display()
+
+array_2 = [1, 2, 3, 5, 7, 4, 6, 8, 9, 10]
+
+a2 = Check_Array(array_2)
+a2.display()
+
--- DSA_Array_Easy/DSA_Consecutive_Ones.py
+++ DSA_Array_Easy/DSA_Consecutive_Ones.py
@@ -29,3 +29,45 @@ array = [1, 2, 1, 1, 1, 3, 1, 1, 4]
 res = maximum_consecutive(array)
 
 print(f"Maximum consecutive 1's in array : {res}.")
+
+
+
+'''IMPLEMENTING OOPs CONCEPTS.'''
+
+class Consecutive:
+    
+    def __init__(self, arr):
+        self.arr = arr
+    
+    def max_consecutive(self):
+        
+        num = len(self.arr)
+        count = 0
+        max_count = 0
+
+        for i in range(num):
+            
+            if self.arr[i] == 1:
+                count += 1
+                
+            else:
+                count = 0
+                
+            max_count = max(max_count, count)
+
+        return max_count
+    
+    def display(self):
+        result = self.max_consecutive()
+    
+        print(f"Given array : {self.arr}")
+        
+        print(f"Maximum consecutive 1's : {result}")
+        
+
+# Main/Driver code :
+
+array = [1, 2, 1, 1, 1, 3, 1, 1, 4]
+
+answer = Consecutive(array)
+answer.display()
