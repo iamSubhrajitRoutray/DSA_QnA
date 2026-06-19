4d819534a6e8d6ceefa1b517f8cccaf549ccf5ef
Subhrajit Routray <theroutraysubhrajit@gmail.com>
2026-06-19 23:29:54 +0530

    "Debugging & Scrutinizing."

--- DSA_Array_Easy/DSA_Longest_SubArray.py
+++ DSA_Array_Easy/DSA_Longest_SubArray.py
@@ -17,12 +17,63 @@ def sub_array(arr, k):
             if total == k:
                 max_len = max(max_len, j - i + 1)
                 
-    print(max_len)
+    return max_len
     
 
 # Main/Driver code : 
 array = [10, 5, 2, 7, 1, 9]
 
 k = 15
+
+ans = sub_array(array, k)
  
-sub_array(array, k)
+print(f"Longest subaaray : {ans}")
+
+
+
+'''IMPLEMENTING OOPs CONCEPTS.'''
+
+class Sub_Array:
+    
+    def __init__(self,arr,k):
+        self.arr = arr
+        self.k = k
+        
+    def longest(self):
+        
+        num = len(self.arr)
+        
+        max_len = 0
+        
+        for i in range(num):
+        
+            total_sum = 0
+        
+            for j in range(i, num):
+        
+                total_sum += self.arr[j]
+        
+                if total_sum == self.k:
+        
+                    max_len = max(max_len, j - i + 1)
+                    
+        return max_len
+    
+    def display(self):
+        
+        print(f"Given array : {self.arr}")
+        
+        answer = self.longest()
+        
+        print(f"Longest subarray with sum {self.k} : {answer}")
+        
+
+
+array = [10, 5, 2, 7, 1, 9]
+
+k = 15
+
+result = Sub_Array(array,k)
+result.display()
+        
+    
