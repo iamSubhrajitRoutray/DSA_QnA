0f13cfb6ab026f925b418653eedd38050cf9d72b
Subhrajit Routray <theroutraysubhrajit@gmail.com>
2026-06-21 12:35:19 +0530

    "Debugging & Scrutinizing."

--- DSA_Array_Easy/DSA_Missing_Number.py
+++ DSA_Array_Easy/DSA_Missing_Number.py
@@ -26,7 +26,6 @@ print(f' Missing number : {num}.')
 
 
 
-
 ''' DIRECT APPROACH -> '''
 
 def missing_number(arr):
@@ -46,3 +45,71 @@ print(array)
 result = missing_number(array)
 
 print(f'Missing number : {result}.')
+
+
+
+
+
+'''IMPLEMENTING OOPs CONCEPTS.'''
+
+
+# BRUTE FORCE APPROACH -> 
+
+class solution:
+    
+    def __init__(self,arr):
+        self.arr = arr
+        
+    def missing(self):
+        
+        n = len(self.arr)
+        
+        for i in range(1, n + 1):
+        
+            if i not in self.arr:
+                return i
+    
+
+    def display(self):
+        
+        print(f"Given array : {self.arr}")
+        
+        ans = self.missing()
+        
+        print(f"The missing number : {ans}")
+        
+# Main/Driver code :
+array = [4, 6, 1, 8, 2, 3, 7]
+
+res = solution(array)
+res.display()
+
+
+
+# DIRECT APPROACH -> 
+
+class missing:
+
+    def __init__(self,arr):
+        self.arr = arr
+
+    def number(self):
+
+        n = len(self.arr) + 1
+
+        res = (n * (n + 1)) // 2 - sum(self.arr)
+ 
+        return res
+
+    def display(self):
+
+        print(f"Given array : {self.arr}")
+
+        print(f"Missing number : {self.number()}")
+        
+# Main/Driver code :
+
+array = [4, 6, 1, 8, 2, 3, 7]
+
+res = missing(array)
+res.display()
