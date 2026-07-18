9fd7e17921607fae79cc9f56efd6d34e7019da63
Subhrajit Routray <theroutraysubhrajit@gmail.com>
2026-07-19 01:21:55 +0530

    "Debugging and Scrutinizing"

--- DSA_Strings/DSA_Larget_Odd.py
+++ DSA_Strings/DSA_Larget_Odd.py
@@ -1,8 +1,8 @@
-'''Largest Odd Number in a String.
+'''Largest Odd Number in a String
 Q)
 Given a string s, representing a large integer, the task is to return the largest-valued odd integer (as a string) that is a substring of the given string s.
 The number returned should not have leading zero's.
-But the given input string may have leading zero.'''
+But the given input string may have leading zero'''
 
 
 
