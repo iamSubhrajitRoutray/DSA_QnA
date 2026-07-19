3cb2d1014cc76e52ecf5d21c4c70fb954fc4820b
Subhrajit Routray <theroutraysubhrajit@gmail.com>
2026-07-19 21:18:16 +0530

    "To find Longest Common Prefix in a given List."

--- DSA_Strings/DSA_Longest_Common_Prefix.py
+++ DSA_Strings/DSA_Longest_Common_Prefix.py
@@ -2,3 +2,42 @@
 Q)
 Write a function to find the longest common prefix string amongst an array of strings.
 If there is no common prefix, return an empty string "".'''
+
+
+def longest_common_prefix(s):
+    
+    result = ""
+    
+    base = s[0]
+    
+    for i in range(len(base)):
+        for word in s[1 : ]:
+            
+            if i == len(word) or word[i] != base[i]:
+                return result
+            
+        result = result + base[i]
+        
+    return result
+
+
+
+# Main/Driver code:
+
+words = ["flower", "flow", "flight"]
+
+answer = longest_common_prefix(words)
+
+print(f"Common Prefix in given List: {answer}")
+
+
+
+# Time Complexity : O(m x n)
+
+# Space Complexity : O(1) or O(m); where m is the lenght of the longest prefix.
+        
+            
+    
+    
+    
+    
